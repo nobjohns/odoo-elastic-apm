12438666ba151a43284e23fe5c477f8d828ea24d
nobjohns <noble.johnson@nobjohns.com>
2022-11-27 12:00:46 +0530

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from . import base
+from . import http
+from . import ir_http
+from . import models
+from . import ir_cron
---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Odoo Elastic APM Integration",
+    "version": "0.1.0",
+    "author": "noble.johnson@ishafoundation.org",
+    "license": "GPL-3",
+    "category": "Extra Tools",
+    "website": "www.ishafoundation.org",
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "external_dependencies": {
+        "python": ["elasticapm"],
+    },
+    "post_load": False
+}
